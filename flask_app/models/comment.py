a7906c22c88280702606f1dc4334e5b253e4534d
Joshua Korn <111288197+Joshua-Korn@users.noreply.github.com>
2023-03-31 20:52:45 -0400

    Update comment.py

--- flask_app/models/comment.py
+++ flask_app/models/comment.py
@@ -31,14 +31,6 @@ class Comment:
         results = connectToMySQL(db).query_db(query, data)
         return cls(results[0])
 
-
-    def get_comment(cls, data):
-        query = "SELECT * FROM comments where id = %(id)s;"
-        results = connectToMySQL(db).query_db(query, data)
-        return cls(results[0])
-        
-    
-
     @classmethod
     def delete(cls,data):
         query = "DELETE FROM comments WHERE id = %(id)s;"
